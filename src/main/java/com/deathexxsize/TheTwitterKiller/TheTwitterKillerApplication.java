e6f8d12cf79b7121cdc5b8f67de92af21e593879
Deathexxsize <nurjigit006abdyganyev@gmail.com>
2025-07-30 13:18:57 +0600

    connect

---
+++ src/main/java/com/deathexxsize/TheTwitterKiller/TheTwitterKillerApplication.java
@@ -0,0 +1,14 @@
+package com.deathexxsize.TheTwitterKiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTwitterKillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheTwitterKillerApplication.class, args);
+		System.out.println("GOOD");
+	}
+
+}
---
+++ src/test/java/com/deathexxsize/TheTwitterKiller/TheTwitterKillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deathexxsize.TheTwitterKiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTwitterKillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
